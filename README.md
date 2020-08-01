0f72a00c56d4b30da0727356edc85a917e32a7d2
Cha14ka <taitor@yandex.ru>
2020-08-02 01:17:53 +0300

    Deleted GUI-Trash

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-# reddit-video-downloader
-A simple python app with a GUI to download reddit videos on Linux (never tested on Windows, might work)
+# reddit-video-downloader-v2
+A simple python script to download reddit videos on Linux and Windows
 
-If there is already an URL in the clipboard it will use it, try to download the reddit video and open the folder that contains the output video. It will bypass the GUI.
-
-Else : it starts the gui and you can download whatever you want from reddit.
-
-There is no progress bar and no way to know what went wrong when it does (and it does) unless you start it with a terminal.
-
-It requires tkinter and clipboard.
-
-Here is a screenshot to show you the wonderful GUI that comes with it :
-![screenshot](https://i.imgur.com/NOkrFTZ.png)
+You need curl and ffmpeg
--- redditvideos.py
+++ redditvideos.py
@@ -1,82 +1,35 @@
-#!/usr/bin/env python
-from tkinter import Entry,Button,Tk,END
-import tkinter.font as tkFont
-import tkinter.ttk
+#!/usr/bin/python3
 import requests
 import os
-import subprocess
-import clipboard
-from urllib.request import urlopen, URLError
+import sys
+import time
 
-real_path = os.path.dirname(os.path.realpath(__file__))
-os.system("mkdir " + real_path+"/Output")
+if len(sys.argv) == 1:
+	print('You forgot post link')
+	exit()
 
-win = Tk()
-win.attributes("-fullscreen", False)
-myFont = tkFont.Font(family = 'Mono', size = 15, weight = 'bold')
-win.title("Reddit-video-downloader")
-win.geometry('1280x90')
-win.configure(background='black')
+post_url = sys.argv[1] + '.json'
 
-isUrl = False
+headers = {
+	'User-Agent': 'My User Agent 1.0',
+	'From': 'testyouremail@domain.com'
+}
+url = post_url + ".json"
+data = requests.get(url, headers=headers).json()
+media_data = data[0]["data"]["children"][0]["data"]["media"]
+title = data[0]["data"]["children"][0]["data"]["title"]
 
-def validate_web_url(url):
-    try:
-        urlopen(url)
-        return True
-    except:
-        return False
+video_url = media_data["reddit_video"]["fallback_url"]
+audio_url = video_url.split("DASH_")[0] + "DASH_audio.mp4"
 
+os.system("curl -o video.mp4 {}".format(video_url))
+os.system("curl -o audio.mp4 {}".format(audio_url))
 
-def redditDownloader():
-    print(isUrl)
+while True:
+	if os.path.exists('video.mp4') and os.path.exists('audio.mp4'): break
+	time.sleep(0.5)
 
-    if isUrl:
-        post_url = pressepape
-    else:
-        post_url = urlInput.get()
+video_path  = title+".mp4"
 
-    # use UA headers to prevent 429 error
-    headers = {
-        'User-Agent': 'My User Agent 1.0',
-        'From': 'testyouremail@domain.com'
-    }
-    url = post_url + ".json"
-    data = requests.get(url, headers=headers).json()
-    media_data = data[0]["data"]["children"][0]["data"]["media"]
-    title = data[0]["data"]["children"][0]["data"]["title"]
-
-    video_url = media_data["reddit_video"]["fallback_url"]
-    audio_url = video_url.split("DASH_")[0] + "audio"
-
-    os.system("curl -o video.mp4 {}".format(video_url))
-    os.system("curl -o audio.wav {}".format(audio_url))
-
-    video_path  = os.path.join(real_path,"Output",title)+".mp4"
-    folder_path = os.path.join(real_path,"Output")
-
-    os.system("ffmpeg -y -i video.mp4 -i audio.wav -c:v copy -c:a aac -strict experimental '"+video_path+"'")
-    os.system("xdg-open " + folder_path)
-
-def quitter():
-    win.destroy()
-
-pressepape = clipboard.paste()
-
-if isinstance(pressepape, str) and validate_web_url(pressepape):
-    isUrl = True
-    redditDownloader()
-else:
-    pressepape = ""
-
-    bDownload = Button(win, text = "Download", font = myFont, command = redditDownloader)
-    bDownload.place(x=20,   y=20, height = 50, width =120 )
-
-    urlInput = Entry(win)
-    urlInput.place(x=150,   y=20, height = 50, width =980 )
-    urlInput.insert(END,pressepape)
-
-    bQuitter = Button(win, text = "Exit", font = myFont, command = quitter)
-    bQuitter.place(x=1140,   y=20, height = 50, width =120 )
-
-    win.mainloop()
+os.system("ffmpeg -y -i video.mp4 -i audio.mp4 -c:v copy -c:a aac '"+video_path+"'")
+os.system("rm video.mp4;rm audio.mp4") 
